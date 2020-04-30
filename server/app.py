90a2efb66c3a28230e0a8e942a27ca619af2d5c8
czyk <aron.scheffczyk@gmail.com>
2020-04-30 23:03:03 +0200

    starting all over again

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('../client/public/index.html')
+    return render_template('index.html')
 
 
 # Run Server
---
+++ server/static/react/precache-manifest.0444dd537a0b46f25eaf3ab16d8bbd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748a921fb5e794060b8b122f77ada3e4",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "46995cc0f89b56e4371d",
+    "url": "/static/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "500305843aae3fbeed0e",
+    "url": "/static/react/static/js/2.207ba51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/static/js/2.207ba51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46995cc0f89b56e4371d",
+    "url": "/static/react/static/js/main.18b24a34.chunk.js"
+  },
+  {
+    "revision": "944b9a154a52b6a9e218",
+    "url": "/static/react/static/js/runtime-main.de186c02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
