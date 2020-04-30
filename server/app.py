81cf04afb4b3f232585af8ff05b079393af21cfd
czyk <aron.scheffczyk@gmail.com>
2020-04-30 23:48:20 +0200

    added basic user routes

--- server/app.py
+++ server/app.py
@@ -4,11 +4,19 @@ import os
 # Init app
 app = Flask(__name__)
 
-
+# Serve Application
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# User Routes
+@app.route('/api/user', methods=['GET'])
+def get_user():
+    return 'Route to get a user'
+
+@app.route('/api/user', methods=['POST'])
+def post_user():
+    return 'Route to create a user'
 
 # Run Server
 if __name__ == '__main__':
