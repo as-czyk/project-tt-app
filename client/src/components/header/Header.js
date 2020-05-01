2f0ca24baed0f88ebfbc88c1d992ceca82600c12
czyk <aron.scheffczyk@gmail.com>
2020-05-01 13:19:20 +0200

    added basic navigation; add pages for trips and home

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <div className='header__container'>
+      <i className='fas fa-bars fa-3x' />
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
