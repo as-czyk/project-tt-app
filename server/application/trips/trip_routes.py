1a172b728ba794162f2b91f2f984ff740a5726fc
Yannik Suhre <yannik@familie-suhre.de>
2020-05-03 21:18:50 +0200

    Ich hänge fest

--- server/application/trips/trip_routes.py
+++ server/application/trips/trip_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 from flask import current_app as app
 import pymongo
 from flask import Flask, request, jsonify, render_template
+from bson import Binary, Code
+from bson.json_util import dumps
 
 client = pymongo.MongoClient(
     "mongodb+srv://yannik:techtalents2020@connext-en64e.mongodb.net/test?retryWrites=true&w=majority")
@@ -11,18 +13,17 @@ trips_bp = Blueprint('trips_bp', __name__,
                      template_folder='templates',
                      static_folder='static')
 
-@trips_bp.route('/api/journey/', methods=['GET'])
-def get_journey():
-    #query = request.args
-    journeylist = client.table.journey
-    journeys = []
-    journey = journeylist.find()
-    for j in journey:
-        j.pop("_id")
-        journeys.append(j)
-    #return "This is a test"
-    return jsonify(journeys), 200
-
+@trips_bp.route('/api/trip/<trip_id>', methods=['GET'])
+def get_journey(trip_id):
+    return trip_id
+    # table_journey_db = client.table.journey
+    # journey_test = table_journey_db.find({'journey_id': "'" + trip_id + "'"})
+    # journeys = []
+    # for journey in journey_test:
+    #      #journey.pop("journey_id")
+    #      journeys.append(journey)
+    # # #return "This is a test"
+    # return dumps(journeys)
 
     # @app.route('/api/journey', methods=['POST'])
 # def post_jounrey():
