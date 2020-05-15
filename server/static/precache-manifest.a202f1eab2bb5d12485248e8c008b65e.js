9825bbd7876800523c53ab712c7d34dcccdfb107
czyk <aron.scheffczyk@gmail.com>
2020-05-15 16:05:58 +0200

    Co-authored-by: ysuhre@uni-mainz.de <ysuhre@uni-mainz.de>

---
+++ server/static/precache-manifest.a202f1eab2bb5d12485248e8c008b65e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d938895e05c6346453ea60cb7aa43dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bce47cdebce6c2bd74e8",
+    "url": "./static/css/main.eb918319.chunk.css"
+  },
+  {
+    "revision": "73f1cb26ca58c3d02717",
+    "url": "./static/js/2.5576b17f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.5576b17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce47cdebce6c2bd74e8",
+    "url": "./static/js/main.5f8b4316.chunk.js"
+  },
+  {
+    "revision": "9163a5cc4999f9e05246",
+    "url": "./static/js/runtime-main.1a875078.js"
+  },
+  {
+    "revision": "008bf3c64bbe579474dd8f52904b363d",
+    "url": "./static/media/Logo.008bf3c6.svg"
+  },
+  {
+    "revision": "f30f551e30ba7df8abbecaba9a50ae41",
+    "url": "./static/media/eintracht.f30f551e.jpg"
+  },
+  {
+    "revision": "a611bcffb4275957767e3809943b7a78",
+    "url": "./static/media/worldclubdome.a611bcff.jpg"
+  }
+]);
