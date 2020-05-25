f629de8a8aca97001089e0d942e3bdc615faba28
czyk <aron.scheffczyk@gmail.com>
2020-05-25 17:42:19 +0200

    adjusted footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,16 +3,7 @@ import React from 'react';
 import './footer.scss';
 
 const Footer = () => {
-  return (
-    <div className='footer__wrapper'>
-      <div className='footer__content'>
-        <h1>Footer-Content 1</h1>
-      </div>
-      <div className='footer__content'>
-        <h1>Footer-Content 2</h1>
-      </div>
-    </div>
-  );
+  return <div className='footer__wrapper'>&copy; Eventway</div>;
 };
 
 export default Footer;
