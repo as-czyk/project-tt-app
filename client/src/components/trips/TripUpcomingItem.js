5fa5b9653f91172e4c44fbda3ea64d186bb0db07
czyk <aron.scheffczyk@gmail.com>
2020-06-08 15:13:09 +0200

    added upcoming items; added onClick for journeys

--- client/src/components/trips/TripUpcomingItem.js
+++ client/src/components/trips/TripUpcomingItem.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import TripContext from '../../context/trip/TripContext';
 
 const TripUpcomingItem = (props) => {
+  const tripContext = useContext(TripContext);
   const { event } = props;
+  const { loadTrips } = tripContext;
 
-  useState({
+  const state = useState({
     id: event.event_id,
   });
 
+  const onClick = (state) => {
+    loadTrips(state[0].id);
+  };
+
   return (
-    <div className='tripsUpcoming__item__container'>
-      <p>{event.event_name}</p>
-      <p>{event.client_id}</p>
+    <div
+      onClick={() => {
+        onClick(state);
+      }}
+      className='tripsUpcoming__item__container'
+    >
+      <p>{event.club_id_home}</p>
+      <p>Vs</p>
+      <p>{event.club_id_away}</p>
     </div>
   );
 };
