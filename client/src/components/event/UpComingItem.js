0b984fc9d3c77ebef57fa81fcc59af4baeac27ef
czyk <aron.scheffczyk@gmail.com>
2020-06-17 17:27:22 +0200

    fixed path bug for images; deployment

--- client/src/components/event/UpComingItem.js
+++ client/src/components/event/UpComingItem.js
@@ -6,13 +6,13 @@ const UpComingItem = (props) => {
   return (
     <div className='upcoming__game__box'>
       <img
-        src={`/images/${event.club_id_home}.png`}
+        src={`/static/images/${event.club_id_home}.png`}
         alt='Home Team'
         className='upcoming__logo__wrapper'
       />
       <p style={{ fontSize: '30px' }}>:</p>
       <img
-        src={`/images/${event.club_id_away}.png`}
+        src={`/static/images/${event.club_id_away}.png`}
         alt='Away Team'
         className='upcoming__logo__wrapper'
       />
